7a713a619e330d120b22cad7ae37cb8af90cbe2a
grishinaanet <aniaanet@yandex.ru>
2023-09-06 02:22:22 +0300

    добавлено решение

---
+++ 4 этап/Program.cs
@@ -0,0 +1,26 @@
+﻿﻿string[] array = new string[6] {"123", "кот", "hello", "world", "мяу","456"};
+Console.WriteLine("Дан массив: 123, кот, hello, world, мяу,456");
+string[] newArray = new string[array.Length];
+void SecondArray(string[] array, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        newArray[count] = array[i];
+        count++;
+        }
+    }
+}   
+void PrintArray(string[] array)
+{
+    Console.Write("Получен массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array, newArray);
+PrintArray(newArray);
